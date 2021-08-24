71b58cf7f536755ced69d96adb1d94bafad87662
Akshay Jain <83016730+akshayjain27989@users.noreply.github.com>
2021-08-24 16:26:10 +0530

    Update wcat_commands.js

--- wcat/wcat_commands.js
+++ wcat/wcat_commands.js
@@ -128,7 +128,7 @@ function addSequenceNumbers(content){
     let count =1;
     for(let i=0 ; i<arr.length;i++){
         if(arr[i]!=""){
-            arr[i]=(count+1) +" " + arr[i];
+            arr[i]=(count) +" " + arr[i];
             count++;
         }
     }
